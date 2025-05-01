768594c4bf34a96b6d96620df62ec77cd0075d33
Sarijen <sajskyv@gmail.com>
2025-05-01 12:16:40 +0200

    Updated to latest config

---
+++ dotfiles/backup_dotfiles.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# i3
+cp ~/.config/i3/config ./i3/config
+
+# Polybar
+cp ~/.config/polybar/config.ini ./polybar/config
+cp ~/.config/polybar/spotify_status.py ./polybar/spotify_status.py
+
+# Picom
+cp ~/.config/picom.conf ./picom.conf
+
+# Terminator
+cp ~/.config/terminator/config ./terminator/config
