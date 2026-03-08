583a62f54a4f489ac77ead6300cc3c8496fa67cf
Sarijen <sajskyv@gmail.com>
2026-03-08 22:07:56 +0100

    Update to latest config

--- dotfiles/backup_dotfiles.sh
+++ dotfiles/backup_dotfiles.sh
@@ -8,7 +8,6 @@ cp ~/.config/i3/config ./i3/config
 
 # Polybar
 cp ~/.config/polybar/config.ini ./polybar/config
-cp ~/.config/polybar/spotify_status.py ./polybar/spotify_status.py
 
 # Rofi
 cp ~/.config/rofi/config.rasi ./rofi/config.rasi
@@ -19,6 +18,5 @@ cp ~/.config/picom.conf ./picom.conf
 # Terminator
 cp ~/.config/terminator/config ./terminator/config
 
-# Htop
-
+# htop
 cp ~/.config/htop/htoprc ./htoprc
